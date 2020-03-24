91ecc3c91512a3391b720357e082bbfd704d3538
Hattrick-3 <48647942+Hattrick-3@users.noreply.github.com>
2020-03-24 14:02:18 +0300

    Add files via upload

--- my test/script.js
+++ my test/script.js
@@ -1,6 +1,7 @@
 var input = document.querySelector('.input');
 var area = document.querySelector('textarea');
 
-input.addEventListener('keyup', function () {
-    area.value = input.value
+input.addEventListener('keyup', function (ev) {
+    area.value = input.value + ev.key + ev.keyCode;
+    console.log(ev)
 })
